edf5ab3ef5a4d972034ecfe2201c1c6da2fc963e
Arash Rowshan <arash.rowshan@gmail.com>
2018-07-23 23:38:57 -0400

    changing to usds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 // End of Components
 
-import { AppRoutingModule } from 'src/app/app.routing';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppRoutingModule } from "src/app/app.routing";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 // Material Modules
 import {
@@ -49,21 +49,21 @@ import {
   MatTooltipModule,
   MatTreeModule,
   MatFormFieldModule
-} from '@angular/material';
+} from "@angular/material";
 
 // Components
-import { SignupComponent } from './signup/signup.component';
-import { SigninComponent } from './signin/signin.component';
-import { HomeComponent } from './home/home.component';
-import { GridLayoutComponent } from './grid-layout/grid-layout.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { LoginLogoutButtonComponent } from './login-logout-button/login-logout-button.component';
-import { SideMainLayoutComponent } from './layouts/side-main-layout/side-main-layout.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ItemComponent } from './item/item.component';
-import { SideNavLayoutComponent } from './layouts/side-nav-layout/side-nav-layout.component';
-import { HomeRoutingModule } from './home/home.routing';
+import { SignupComponent } from "./signup/signup.component";
+import { SigninComponent } from "./signin/signin.component";
+import { HomeComponent } from "./home/home.component";
+import { GridLayoutComponent } from "./grid-layout/grid-layout.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
+import { LoginLogoutButtonComponent } from "./login-logout-button/login-logout-button.component";
+import { SideMainLayoutComponent } from "./layouts/side-main-layout/side-main-layout.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { ItemComponent } from "./item/item.component";
+import { SideNavLayoutComponent } from "./layouts/side-nav-layout/side-nav-layout.component";
+import { HomeRoutingModule } from "./home/home.routing";
 
 // End of Material Modules
 
@@ -87,7 +87,6 @@ import { HomeRoutingModule } from './home/home.routing';
     BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
-    HomeRoutingModule,
     ReactiveFormsModule,
     FormsModule,
     // Materials
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { ApiService } from '../apis/api.service';
+import { AuthService } from '../auth-guard/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -8,22 +10,30 @@ import { ApiService } from '../apis/api.service';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-
   signupForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private apiService: ApiService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private apiService: ApiService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    if (this.authService.isLoggedIn) {
+      this.router.navigateByUrl('/home');
+    }
     this.signupForm = this.formBuilder.group({
       name: ['', [Validators.required]],
       username: ['', [Validators.required]],
       email: ['', [Validators.required]],
-      password: ['', [Validators.required]],
-   });
+      password: ['', [Validators.required]]
+    });
   }
 
   onSignupSubmit(signupForm) {
-    this.apiService.signup(signupForm).subscribe(res => console.log(res));
+    this.apiService.signup(signupForm).subscribe(res => {
+      this.router.navigateByUrl('/signin');
+    });
   }
-
 }
