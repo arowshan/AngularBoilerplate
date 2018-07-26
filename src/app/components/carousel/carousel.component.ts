a554bf8228186a3dc9931352f099ec6784188aaf
Arash Rowshan <arash.rowshan@gmail.com>
2018-07-26 11:05:05 -0400

    carousel basics implemented

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
+  images = [
+    'http://www.gstatic.com/webp/gallery/1.jpg',
+    'http://www.gstatic.com/webp/gallery/2.jpg',
+    'http://www.gstatic.com/webp/gallery/3.jpg'
+  ];
 
-  constructor() { }
+  currentImageIndex;
+
+  constructor() {}
 
   ngOnInit() {
+    // Check if any images, set first one
+    if (this.images.length > 0) {
+      this.currentImageIndex = 0;
+    }
   }
 
+  next() {
+    if (this.currentImageIndex < this.images.length - 1) {
+      this.currentImageIndex++;
+    }
+  }
+
+  previous() {
+    if (this.currentImageIndex > 0) {
+      this.currentImageIndex--;
+    }
+  }
 }
