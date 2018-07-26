18abbb2fb090177b2c402fc0569bd7ffa6f2cdd2
Arash <arowshan@metrostarsystems.com>
2018-07-26 15:46:34 -0400

    adds units tests to pipes and http service

--- src/app/pipes/truncate.pipe.spec.ts
+++ src/app/pipes/truncate.pipe.spec.ts
@@ -1,8 +1,25 @@
 import { TruncatePipe } from './truncate.pipe';
 
 describe('TruncatePipe', () => {
+  let pipe: TruncatePipe;
+
+  beforeEach(() => {
+    pipe = new TruncatePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new TruncatePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('pipe to receive two arguments and return a string', () => {
+    expect(typeof pipe.transform('', [])).toBe('string');
+  });
+
+  it('pipe to truncate text by the length given', () => {
+    expect(pipe.transform('abcde', ['2', ''])).toBe('ab');
+  });
+
+  it('pipe to add ... if no 2nd arg given in array', () => {
+    expect(pipe.transform('abcde', ['2'])).toBe('ab...');
   });
 });
--- src/app/services/http-client/http-client.service.spec.ts
+++ src/app/services/http-client/http-client.service.spec.ts
@@ -1,18 +1,38 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { HttpClientService } from './http-client.service';
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientModule,
+  HttpClient,
+  HttpHandler,
+  HttpErrorResponse
+} from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+import { Data } from '../../../../node_modules/@angular/router';
 
 describe('HttpClientService', () => {
   let service: HttpClientService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let testUrl = '/data';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [HttpClientService],
-      imports: [HttpClientModule]
+      imports: [HttpClientTestingModule]
     });
 
     service = TestBed.get(HttpClientService);
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
 
   it('should be created', inject(
@@ -40,11 +60,53 @@ describe('HttpClientService', () => {
 
   it('should call setup url on a delete request', () => {
     const spy = spyOn(service, 'setupUrl');
-    service.get('test');
+    service.delete('test');
     expect(spy).toHaveBeenCalled();
   });
 
-  it('should return a url string', () => {
-    expect(typeof service.setupUrl('test')).toBe('string');
+  it('should handle error', () => {
+    expect(service.get).toBe('string');
+  });
+
+  it('can test HttpClient.get', () => {
+    const testData: Data = { name: 'Test Data' };
+
+    // Make an HTTP GET request
+    httpClient.get<Data>('/data').subscribe(data =>
+      // When observable resolves, result should match test data
+      expect(data).toEqual(testData)
+    );
+
+    // The following `expectOne()` will match the request's URL.
+    // If no requests or multiple requests matched that URL
+    // `expectOne()` would throw.
+    const req = httpTestingController.expectOne('/data');
+
+    // Assert that the request is a GET.
+    expect(req.request.method).toEqual('GET');
+
+    // Respond with mock data, causing Observable to resolve.
+    // Subscribe callback asserts that correct data was returned.
+    req.flush(testData);
+
+    // Finally, assert that there are no outstanding requests.
+    httpTestingController.verify();
+  });
+
+  it('can test for 404 error', () => {
+    const emsg = 'deliberate 404 error';
+
+    httpClient.get<Data[]>(testUrl).subscribe(
+      data => fail('should have failed with the 404 error'),
+      (error: HttpErrorResponse) => {
+        expect(error.status).toEqual(404, 'status');
+        expect(error.error).toEqual(emsg, 'message');
+      }
+    );
+
+    const req = httpTestingController.expectOne(testUrl);
+
+    // Respond with mock error
+    req.flush(emsg, { status: 404, statusText: 'Not Found' });
   });
 });
